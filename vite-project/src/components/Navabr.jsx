e47958c411b615909c674fa469096311987087bc
unknown <lizzybumpers11@gmail.com>
2024-02-13 12:57:17 -0600

    added the navbar/pages and made the 4 points for nav

---
+++ vite-project/src/components/Navabr.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function NavBar() {
+    return (
+        <nav>
+            <ul>
+                <li>About</li>
+                <li>Portfolio</li>
+                <li>Contact</li>
+                <li>Resume</li>
+            </ul>
+        </nav>
+    )
+}
+export default NavBar
