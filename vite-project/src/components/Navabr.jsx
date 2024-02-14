6341f30c369fe3a5cdaffd7aabef719bbbafc43e
unknown <lizzybumpers11@gmail.com>
2024-02-14 12:29:29 -0600

    added links and routing to links

--- vite-project/src/components/Navabr.jsx
+++ vite-project/src/components/Navabr.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 function NavBar() {
     return (
         <nav>
             <ul>
-                <li>About</li>
-                <li>Portfolio</li>
-                <li>Contact</li>
-                <li>Resume</li>
+                <li>
+                    <Link to='/about'>About</Link>
+                </li>
+                <li>
+                    <Link to='/portfolio'>Portfolio</Link>
+                </li>
+                <li>
+                    <Link to='/contact'>Contact</Link>
+                </li>
+                <li>
+                    <Link to='/resume'>Resume</Link>
+                </li>
             </ul>
         </nav>
     )
 }
+
 export default NavBar
