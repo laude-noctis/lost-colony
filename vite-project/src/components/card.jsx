3952e52462983169442c5e9e5010668f7901d40e
unknown <lizzybumpers11@gmail.com>
2024-02-18 20:35:31 -0600

    added the info for each portfolio card

---
+++ vite-project/src/components/card.jsx
@@ -0,0 +1,14 @@
+export default function Card(props) {
+    return (
+        <div className="card">
+                <div className="port-card">
+                    <a href={props.code} target="_blank">
+                        <i className="fab fa-github icon port-icon"></i>
+                    </a>
+                    <h2 className="port-title">
+                        <a href={props.webpage} target="_blank">{props.title}</a>
+                    </h2>
+                </div>
+            </div>
+    )
+}
--- vite-project/src/pages/PortfolioPage.jsx
+++ vite-project/src/pages/PortfolioPage.jsx
@@ -1,19 +1,38 @@
 import React from 'react';
+import Card from '../components/card';
+
+const eternalEngine = {
+    code: "https://github.com/laude-noctis/eternal-engine",
+    webpage: "https://eternal-engine-f89c83326069.herokuapp.com/",
+    title: "Eternal-Engine",
+}
+
+const Atmosphoria = {
+    code: "https://github.com/alexilson/atmosphoria",
+    webpage: "https://alexilson.github.io/atmosphoria/",
+    title: "Atmosphoria",
+}
+
+const whiteJungle = {
+    code: "https://github.com/laude-noctis/white-jungle",
+    webpage: "https://white-jungle-ef3a1e24340d.herokuapp.com/",
+    title: "White-Jungle",
+}
+
+const crazyGadget = {
+    code: "https://github.com/laude-noctis/crazy-gadget",
+    webpage: "https://laude-noctis.github.io/crazy-gadget/",
+    title: "Crazy-Gadget",
+}
 
 export const PortfolioPage = () => {
     return (
         <div>
             <h1>Portfolio</h1>
-            <div className="card">
-                <div className="port-card">
-                    <a href="https://github.com/laude-noctis/eternal-engine" target="_blank">
-                        <i className="fab fa-github icon port-icon"></i>
-                    </a>
-                    <h2 className="port-title">
-                        <a href="https://eternal-engine-f89c83326069.herokuapp.com/" target="_blank">Eternal-Engine</a>
-                    </h2>
-                </div>
-            </div>
+             <Card code={eternalEngine.code} webpage={eternalEngine.webpage} title={eternalEngine.title} />
+             <Card code={Atmosphoria.code} webpage={Atmosphoria.webpage} title={Atmosphoria.title} />
+             <Card code={whiteJungle.code} webpage={whiteJungle.webpage} title={whiteJungle.title} />
+             <Card code={crazyGadget.code} webpage={crazyGadget.webpage} title={crazyGadget.title} />
         </div>
     );
 }
