dca38cb36100cc0fcc900121004630fd0d142664
unknown <lizzybumpers11@gmail.com>
2024-02-18 20:38:09 -0600

    reorganized components folder

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css'
-import NavBar from './components/Navbar.jsx'
-import Footer from './components/Footer.jsx'
+import NavBar from './components/NavBar/Navbar.jsx'
+import Footer from './components/Footer/Footer.jsx'
 import { Route, Routes } from 'react-router-dom';
 import { AboutPage, ContactPage, PortfolioPage, ResumePage } from './pages';
 
