3ed26f46c575479ae1bc82ca1d8ee343f2e125b2
unknown <lizzybumpers11@gmail.com>
2024-02-18 20:14:19 -0600

    Got the layout for portfolio cards ready

--- vite-project/src/pages/PortfolioPage.jsx
+++ vite-project/src/pages/PortfolioPage.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
 export const PortfolioPage = () => {
-    return <div>
-        <h1>Portfolio</h1>
-    </div>;
+    return (
+        <div>
+            <h1>Portfolio</h1>
+            <div className="card">
+                <div className="port-card">
+                    <a href="https://github.com/laude-noctis/eternal-engine" target="_blank">
+                        <i className="fab fa-github icon port-icon"></i>
+                    </a>
+                    <h2 className="port-title">
+                        <a href="https://eternal-engine-f89c83326069.herokuapp.com/" target="_blank">Eternal-Engine</a>
+                    </h2>
+                </div>
+            </div>
+        </div>
+    );
 }
