2f53a35dcaff76b38f95038dd1d5cfdf7562689a
unknown <lizzybumpers11@gmail.com>
2024-02-21 11:17:52 -0600

    grammar

--- vite-project/src/pages/AboutPage.jsx
+++ vite-project/src/pages/AboutPage.jsx
@@ -14,9 +14,9 @@ export const AboutPage = () => {
             <p>Howdy, my name is Lizzie Bumpers and I am currently studying to become a Full-Stack-Developer.
                 Throughtout my time studying, I have enjoyed working on the back-end side. I liked working on the api routing and the server side code.
                 I am interested in video game development and hope to one day transition into that side of the tech industry.</p>
-            <p>One of the group projects I was a part of, we built a weather app that retieved the current weather status of your area in an accent of your choice.
+            <p>On one of the group projects I was a part of, we built a weather app that retieved the current weather status of your area in an accent of your choice.
                 There were four choices to pick from for the accent. We used ChatGPT to and a WeatherAPI to help develop the app. </p>
-            <p>Another project I did solo was a Tech Blog. I created it to be able to have a place to be able discuss all things tech related.
+            <p>An example of a project I did on my own was a Tech Blog. I created it to be able to have a place to be able discuss all things tech related.
                 I used an express server alongside the sequelize database to develop this application. This is also where I really got into back-end api routing.
             </p>
         </div>;
